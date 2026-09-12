9646d6ac2e4884e2ee5a28995d3aa20d0060424b
Kilombino <kilombino@proton.me>
2026-09-12 08:45:58 +0000

    mempool: AlphaPool blocks show [AlphaPool logo] + coinbase author + [DATUM logo]

--- backend/src/api/blocks.ts
+++ backend/src/api/blocks.ts
@@ -357,7 +357,7 @@ class Blocks {
           minerNames: null,
         };
 
-        if (['OCEAN', 'Lazarus', 'DATUM miners', 'TIDES', 'RIPTIDE', 'CONVOY', 'Convoy', 'CONVOYMining', 'PYBLOCK WAVICLES', 'iohzrd'].includes(extras.pool.name)) {
+        if (['OCEAN', 'Lazarus', 'DATUM miners', 'TIDES', 'RIPTIDE', 'CONVOY', 'Convoy', 'CONVOYMining', 'PYBLOCK WAVICLES', 'iohzrd', 'AlphaPool'].includes(extras.pool.name)) {
           extras.pool.minerNames = reorderMinerNames(extras.pool.name, parseDATUMTemplateCreator(extras.coinbaseRaw));
         }
 
